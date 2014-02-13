8e60a48fec20259d0583efe594f377c7b0981fc8
SuprDewd <suprdewd@gmail.com>
2014-02-13 05:06:34 +0000

    Changed how input to rules is handled and added more general rules.

--- permutation_sets/__init__.py
+++ permutation_sets/__init__.py
@@ -1,7 +1,8 @@
 
-from input_permutation_set import InputPermutationSet
+from input import Input
 from permutation_set import PermutationSet
 from point import Point
 from simple_generating_rule import SimpleGeneratingRule
+from generating_rule import GeneratingRule
 from static_permutation_set import StaticPermutationSet
 
