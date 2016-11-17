61cee67616eafbd36ce07811e5343f520d88080c
Henning Ulfarsson <ulfarsson@gmail.com>
2016-11-17 14:04:57 +0000

    Looking at 11x4 bases that fail in 6x6 rules

---
+++ examples/classical_11x4/1324_1432_2341_2413_2431_3142_3214_3241_4123_4132_4213.py
@@ -0,0 +1,47 @@
+from __future__ import print_function
+from permuta import *
+import permstruct
+import permstruct.dag
+from permstruct import *
+from permstruct.dag import taylor_dag
+
+import sys
+
+is_classical = True
+
+# STATUS ================================================ > SUCCESS!
+
+task = '1324_1432_2341_2413_2431_3142_3214_3241_4123_4132_4213'
+patts = [ Permutation([ int(c) for c in p ]) for p in task.split('_') ]
+
+perm_bound    = 9
+verify_bound  = 14
+ignored       = 0
+
+# The dag
+max_len_patt = None
+upper_bound  = None
+remove       = False
+
+# Grids
+max_rule_size = (7, 7)
+max_non_empty = 7
+max_rules     = None
+
+# ------------------------------------------------------------------------------
+
+settings = StructSettings(
+        perm_bound=perm_bound,
+        verify_bound=verify_bound,
+        max_rule_size=max_rule_size,
+        max_non_empty=max_non_empty,
+        max_rules=max_rules,
+        verbosity=StructLogger.INFO)
+# settings.set_input(StructInput.from_avoidance(settings, patts))
+settings.set_input(AvoiderInput(settings, patts))
+settings.set_dag(taylor_dag(settings,
+                    max_len_patt=max_len_patt,
+                    remove=remove,
+                    upper_bound=upper_bound))
+
+exhaustive(settings)
